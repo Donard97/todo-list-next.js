0f30b4159001cdd528523e94aae4a5fed4ccc7fa
Donard97 <donardgolaj@gmail.com>
2024-03-26 18:11:18 +0100

    Import component and styled components of task to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import styles from "./page.module.css";
 import { ReactElement } from "react";
-import { TasksSection } from "./styles/styles";
+import { Button, DeleteButton, TasksParent, TasksSection } from "./styles/styles";
 import TopControls from "./components/Controls/controls";
 import FilterParent from "./components/Filter/filter";
+import Task from "./components/Task/task";
 
 export default function Home():ReactElement {
   return (
@@ -10,6 +11,11 @@ export default function Home():ReactElement {
       <TopControls />
       <TasksSection>
         <FilterParent />
+        <TasksParent>
+        <Task />
+        <Button>Edit</Button>
+        <DeleteButton>Delete</DeleteButton>
+        </TasksParent>
       </TasksSection>
     </main>
   );
